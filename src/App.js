54f0193d98f9b21bcd06fb44756e1b129935b888
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-10-21 22:26:46 -0700

    Added code for testimonial without picture

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { Badges } from "./components/Badges"
 import { Banner } from './components/Banner'
 import { Cards } from './components/Cards'
+import { TestimonialsNoPic } from './components/TesimonialsNoPic'
 import { Testimonials } from './components/Testimonials'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <Cards />
       <Testimonials />
+      <TestimonialsNoPic />
     </div>
   )
 }
